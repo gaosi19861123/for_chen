7d51f76dfde47604d46d827c2fef7d8d3a472b47
gaosi <gaopan0630@gmail.com>
2020-01-16 16:25:46 +0900

    1st_day

---
+++ config.py
@@ -0,0 +1,11 @@
+class Config(object):
+    
+    data_path_aions = "../chen_data/car_august/aions_august/" #where raw data was saved
+    data_path_gs3 = "../chen_data/car_august/gu3_august/"
+    data_path_gs4 = "../chen_data/car_august/gs4_august/"  
+    
+    header_path_gs3 = "gs3_head.txt"
+    header_path_gs4 = "gs4_head.txt"
+    header_path_aions = "aions_head.txt"
+    
+    
